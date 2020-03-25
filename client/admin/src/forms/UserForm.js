bfdefdb2a9c500ffc1b0b62e8e5e578bc7f35aae
Полина Попова <popova_pv@km-union.ru>
2020-03-25 08:39:37 +0300

    Add proper react form

--- client/admin/src/forms/UserForm.js
+++ client/admin/src/forms/UserForm.js
@@ -1,18 +1,42 @@
 import React from 'react';
 
 
-function UserForm () {
-  return (
-    <form>
+class UserForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {firstName: '', secondName: ''};
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('Пользователь сохранен: ' + this.state.firstName + this.state.secondName);
+    // event.preventDefault();
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
       <label>
         Имя:
-        <input type="text" name="name" />
+        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleInputChange} />
+        <br />
         Фамилия:
-        <input type="text" name="name" />
+        <input type="text" name="secondName" value={this.state.secondName} onChange={this.handleInputChange} />
       </label>
-      <button>Save</button>
+      <input type="submit" value="Сохранить"/>
     </form>
-  );
+    )
+  }
+    
 };
 
 export default UserForm;
